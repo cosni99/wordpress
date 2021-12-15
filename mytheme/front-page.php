bd2b537d56b0c5dbfe77b6a02225e7de6ea2def6
cosni99 <cosmosmos99@gmail.com>
2021-12-15 18:22:55 +0900

    front-page newtuika

--- mytheme/front-page.php
+++ mytheme/front-page.php
@@ -13,5 +13,28 @@
   <?php } 
   }?><!--現在の投稿を繰り返すend-->
 
+<!--最新記事のタイトルを何件か表示する-->
+<div class="mycontainer new">
+<?php
+  $args = array(
+    'post_type' => 'post', // 投稿タイプ
+    'posts_per_page' => 3, // 1ページに表示する件数
+    'no_found_rows' => true,  //ページャーを使う時はfalseに。
+    );
+  $the_query = new WP_Query($args);
+  $get_num = $the_query->post_count;//取得した数
+  
+  echo "<h4>最新記事".$get_num."件</h4>";
+  if ( $the_query->have_posts() ) :?>
+
+  <ul>
+  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+      <li><a href="<?php the_permalink() ?>"><?php echo get_the_date().' '; the_title(); ?></a></li>
+  <?php endwhile; ?>
+  <?php wp_reset_postdata(); ?>
+  </ul>
+<?php endif; ?>
+</div>
+<!--最新記事END-->
 <?php get_sidebar(); ?><!--sidebar.php読み込み-->
 <?php get_footer(); ?><!--footer.php読み込み-->
